3f575024db2fdc45cf8f7cdfc9cf7c6369ea8d0a
tuantuana <91653958+tuantuana@users.noreply.github.com>
2026-01-12 11:23:43 +0000

    newsymbel

--- utils/filterBullishAbove10Percent.js
+++ utils/filterBullishAbove10Percent.js
@@ -7,7 +7,7 @@ const filterBullishAbove10Percent = (data) => {
             const buyVolume = c.bv;
             const sellVolume = c.v - c.bv;
             const buyVsSellPercent = ((buyVolume - sellVolume) / c.v) * 100;
-            return percentChange >= 4 ;
+            return percentChange >= 10 ;
 
         });
         if (valid.length > 0) {
